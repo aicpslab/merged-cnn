fb40d91f731733cd40756eb7e789c475bb6228df
emiya cody <34731609+emiyacody@users.noreply.github.com>
2023-12-11 12:37:28 -0500

    Add files via upload

--- engine/netload.py
+++ engine/netload.py
@@ -47,17 +47,17 @@ class Net2(nn.Module):
         return x
 
 
-def err_range(Istar):
+def err_range(Istar, output_size):
     diff_list = []
     n = len(Istar)
     for i in range(n):
         Star1 = Istar[i].toStar()
         vertices = Star1.toPolyhedron()
         if i == 0:
-            for ind in range(1):
+            for ind in range(output_size):
                 diff_list.append([min(vertices[ind, :]), max(vertices[ind, :])])
         else:
-            for ind in range(1):
+            for ind in range(output_size):
                 diff_list[ind] = [min([diff_list[ind][0], min(vertices[ind, :])]),
                                   max([diff_list[ind][1], max(vertices[ind, :])])]
     return diff_list
@@ -86,24 +86,12 @@ def quant_star(input, scale, offset):
     return input
 
 
-def compute_net_diff(pth1, pth2, model1, model2, IM, lb, ub):
-    pth1 = './numer1.pth'
-    pth2 = './numer2.pth'
-    net1_model = Net1()
-    net1 = torch.load(pth1)
-    net2_model = Net2()
-    net2 = torch.load(pth2)
-    # print(net1_model)
-    # summary(net1_model, (1, 6, 6))
-
-    # IM = np.random.rand(6, 6, 1).astype(np.single)
-    lb = -0.05
-    ub = 0.05
-
-    LB = np.zeros((6, 6, 1), dtype=np.single)
-    UB = np.zeros((6, 6, 1), dtype=np.single)
-    LB[0:2, 0:2, :] = lb
-    UB[0:2, 0:2, :] = ub
+def compute_net_diff(model, net1, net2, IM, lb, ub):
+    dim = IM.shape
+    LB = np.zeros(dim, dtype=np.single)
+    UB = np.zeros(dim, dtype=np.single)
+    LB[0, 0, 0] = lb
+    UB[0, 0, 0] = ub
     IM_m = np.concatenate((IM, IM))
     LB_m = np.concatenate((LB, LB))
     UB_m = np.concatenate((UB, UB))
@@ -113,14 +101,17 @@ def compute_net_diff(pth1, pth2, model1, model2, IM, lb, ub):
     p_i = 1
     method = 'exact-star'
     relu_layer = ReLULayer()
-    for name, m in net1_model.named_children():
+    for name, m in model.named_children():
         # print(name, ">>>", m)
         if isinstance(m, nn.Conv2d):
             c_wei1 = net1[name+'.weight'].to(torch.float32)
             c_bias1 = net1[name+'.bias'].to(torch.float32)
             c_wei2 = net2[name + '.weight'].to(torch.float32)
             c_bias2 = net2[name + '.bias'].to(torch.float32)
-            layer = MergedConv2d(c_wei1, c_bias1, c_wei2, c_bias2)
+            pad = m.padding
+            stride = m.stride
+            layer = MergedConv2d(c_wei1, c_bias1, c_wei2, c_bias2,
+                                 pad1=pad, pad2=pad, stride1=stride, stride2=stride)
             Istar = layer.reach(Istar_pre)
         elif isinstance(m, nn.MaxPool2d):
             pad = m.padding
@@ -150,27 +141,18 @@ def compute_net_diff(pth1, pth2, model1, model2, IM, lb, ub):
     fc_last_b2 = np.zeros(output_size)
     layer = FC(fc_last_w1, fc_last_b1, fc_last_w2, fc_last_b2, 'last')
     Istar = layer.reach(Istar_pre)
-    max_range = err_range(Istar)
+    max_range = err_range(Istar, output_size)
 
     return max_range
 
 
-def compute_quant_net_diff(pth1, pth2, model1, model2, IM, lb, ub):
-    # pth1 = './numer1.pth'
-    # pth2 = './numer2.pth'
-    net1_model = model1
-    net1 = torch.load(pth1, map_location=torch.device('cpu'))
-    net2_model = model2
-    net2 = torch.load(pth2, map_location=torch.device('cpu'))
-    # print(net1_model)
-    # summary(net1_model, (1, 6, 6))
+def compute_quant_net_diff(model, net1, net2, IM, lb, ub):
+    # net1 = torch.load(pth1, map_location=torch.device('cpu'))
+    # net2 = torch.load(pth2, map_location=torch.device('cpu'))
 
-    # IM = np.random.rand(6, 6, 1).astype(np.single)
-    lb = -0.05
-    ub = 0.05
-
-    LB = np.zeros((32, 32, 3), dtype=np.single)
-    UB = np.zeros((32, 32, 3), dtype=np.single)
+    dim = np.array(IM.shape)
+    LB = np.zeros(dim, dtype=np.single)
+    UB = np.zeros(dim, dtype=np.single)
     LB[0, 0, 0] = lb
     UB[0, 0, 0] = ub
     IM_m = np.concatenate((IM, IM))
@@ -182,7 +164,7 @@ def compute_quant_net_diff(pth1, pth2, model1, model2, IM, lb, ub):
     p_i = 1
     method = 'exact-star'
     relu_layer = ReLULayer()
-    for name, m in net2_model.named_children():
+    for name, m in model.named_children():
         if isinstance(m, QuantStub):
             scale = net2[name+'.scale'].numpy()
             offset = net2[name+'.zero_point'].numpy()
@@ -227,7 +209,9 @@ def compute_quant_net_diff(pth1, pth2, model1, model2, IM, lb, ub):
             Istar = relu_layer.reach(Istar, method)
         elif isinstance(m, DeQuantStub):
             continue
-        Istar_pre = Istar
+        else:
+            raise Exception("%s layer is undefined!" % name)
+        Istar_pre = Istar.copy()
 
     fc_last_w1 = np.eye(output_size, dtype=np.single)
     fc_last_w2 = -np.eye(output_size, dtype=np.single)
@@ -237,9 +221,70 @@ def compute_quant_net_diff(pth1, pth2, model1, model2, IM, lb, ub):
     Istar_pre = quant_star(Istar_pre, scale, offset)
     Istar = layer.reach(Istar_pre)
 
-    max_range = err_range(Istar)
+    max_range = err_range(Istar, output_size)
 
     return max_range
+
+
+def compute_prune_net_diff(model, net1, net2, IM, lb, ub):
+    dim = IM.shape
+    LB = np.zeros(dim, dtype=np.single)
+    UB = np.zeros(dim, dtype=np.single)
+    LB[0, 0, 0] = lb
+    UB[0, 0, 0] = ub
+    IM_m = np.concatenate((IM, IM))
+    LB_m = np.concatenate((LB, LB))
+    UB_m = np.concatenate((UB, UB))
+    I1_m = ImageStar(IM_m, LB_m, UB_m)
+
+    Istar_pre = I1_m
+    p_i = 1
+    method = 'exact-star'
+    relu_layer = ReLULayer()
+    for name, m in model.named_children():
+        # print(name, ">>>", m)
+        if isinstance(m, nn.Conv2d):
+            c_wei1 = net1[name + '.weight'].to(torch.float32)
+            c_bias1 = net1[name + '.bias'].to(torch.float32)
+            c_wei2 = net2[name + '.weight'].to(torch.float32)
+            c_bias2 = net2[name + '.bias'].to(torch.float32)
+            pad = m.padding
+            stride = m.stride
+            layer = MergedConv2d(c_wei1, c_bias1, c_wei2, c_bias2,
+                                 pad1=pad, pad2=pad, stride1=stride, stride2=stride)
+            Istar = layer.reach(Istar_pre)
+        elif isinstance(m, nn.MaxPool2d):
+            pad = m.padding
+            pad = np.array([pad, pad])
+            stride = m.stride
+            stride = np.array([stride, stride])
+            pool = m.kernel_size
+            pool = np.array([pool, pool])
+            layer = MaxPooling2d(pool, stride, pad, 'maxpool' + str(p_i))
+            Istar = layer.reach(Istar_pre, method)
+            p_i += 1
+        elif isinstance(m, nn.Linear):
+            fc_wei1 = net1[name + '.weight'].to(torch.float32)
+            fc_bias1 = net1[name + '.bias'].to(torch.float32)
+            fc_wei2 = net2[name + '.weight'].to(torch.float32)
+            fc_bias2 = net2[name + '.bias'].to(torch.float32)
+            layer = FC(fc_wei1, fc_bias1, fc_wei2, fc_bias2)
+            Istar = layer.reach(Istar_pre)
+            output_size = m.out_features
+        elif isinstance(m, nn.ReLU):
+            Istar = relu_layer.reach(Istar, method)
+        Istar_pre = Istar
+
+    fc_last_w1 = np.eye(output_size, dtype=np.single)
+    fc_last_w2 = -np.eye(output_size, dtype=np.single)
+    fc_last_b1 = np.zeros(output_size)
+    fc_last_b2 = np.zeros(output_size)
+    layer = FC(fc_last_w1, fc_last_b1, fc_last_w2, fc_last_b2, 'last')
+    Istar = layer.reach(Istar_pre)
+    max_range = err_range(Istar, output_size)
+
+    return max_range
+
 
 if __name__ == '__main__':
     pth1 = '../data/numer1.pth'
